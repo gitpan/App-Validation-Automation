78d08adf4b0b6ace4823af180a14eb12b0de1712
Varun Juyal <libvenus@cpan.org>
2011-12-27 11:20:49 +0000

    Import of LIBVENUS/App-Validation-Automation-0.01 from CPAN.
    gitpan-cpan-distribution: App-Validation-Automation
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         LIBVENUS/App-Validation-Automation-0.01.tar.gz
    gitpan-cpan-author:       LIBVENUS
    gitpan-cpan-maturity:     released

---
+++ lib/App/Validation/Automation/Logging.pm
@@ -0,0 +1,29 @@
+package App::Validation::Automation::Logging;
+
+use Moose::Role;
+use English qw( -no_match_vars );
+
+=head1 NAME
+
+App::Validation::Automation::Logging - Role App::Validation::Automation
+
+Logs message in the log file.The log filename is base script name followed by datestamp and log file extension.Logs for the same day get appened in the same log file.
+
+=cut
+
+requires qw( log_file_handle );
+
+sub log {
+
+    my $self = shift;
+    my $msg  = shift;    
+    local $OUTPUT_AUTOFLUSH  = 1;
+
+    print { $self->log_file_handle } 
+        scalar(localtime(time)).caller()." $msg"."\n" if( $msg );
+
+    return 1;
+
+}
+
+1;
